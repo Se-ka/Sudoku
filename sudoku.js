4022383d5902dbd8c33ad7d03c51ac91c47fb535
Seka Shchek <DoomkaSU@gmail.com>
2015-04-19 18:12:01 +0300

    adding function step by step rendering

--- sudoku.js
+++ sudoku.js
@@ -4,8 +4,10 @@
 
 
 readyFunction = function () {
-
+    v = 0;
+    c = 0;
     sudoku.render();
+    sudoku.stepByStepRendering();
 
 };
 $(readyFunction);
@@ -24,6 +26,18 @@ sudoku = {
         [0,0,0,0,0,0,0,0,0]
     ],
 
+    "solved" : [
+        [1,2,3,4,5,6,7,8,9],
+        [3,0,0,0,0,0,0,0,0],
+        [0,0,0,3,0,0,0,0,0],
+        [0,0,0,0,0,4,0,0,0],
+        [0,0,0,0,0,0,4,0,0],
+        [0,7,0,0,0,0,0,0,0],
+        [0,0,8,0,0,0,0,0,0],
+        [0,5,0,9,0,0,6,0,0],
+        [0,0,0,0,4,0,5,0,0]
+    ],
+
     "render": function () {
 
         var i, k, n;
@@ -39,42 +53,30 @@ sudoku = {
         }
 
 
+    },
+
+    "stepByStepRendering": function () {
+
+                n = this.numbers[c-1][v-1];
+                p = this.solved[c-1][v-1];
+
+                if (n < p) {
+                    $("#" + c + v + " input").val(p);
+                }
+
+        console.log(c,v);
+
+        if (v == 8 && c == 8) {
+            return
+        }
+        if (v <= 7) {
+            v++;
+        } else {
+            if (c <= 7) {
+                v = 0;
+                c++;
+            }
+        }
+        setTimeout(this.stepByStepRendering.bind(this), 1);
     }
-
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
